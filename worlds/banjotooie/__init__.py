cd3c02c5bf1b84121f23dfb8e6f0fc66ec9e52f5
jjjj12212 <mjackson12212@hotmail.com>
2024-02-18 11:57:36 -0800

    Mayahem spelling fix + Honeycombs server side.

--- worlds/banjotooie/__init__.py
+++ worlds/banjotooie/__init__.py
@@ -60,7 +60,8 @@ class BanjoTooieWorld(World):
 
     item_name_groups = {
         "Jiggy": all_group_table["jiggy"],
-        "Jinjo": all_group_table["jinjo"]
+        "Jinjo": all_group_table["jinjo"],
+        "Honeycombs": all_group_table["honeycombs"]
     }
     # location_name_to_id = {}
     
@@ -92,8 +93,6 @@ class BanjoTooieWorld(World):
                 itempool += [self.create_item(name)]
         
         for item in itempool:
-            # if(item.code == 0 or item.code == None):
-            #     continue
             self.multiworld.itempool.append(item)
 
     
@@ -114,7 +113,7 @@ class BanjoTooieWorld(World):
         if(item.code == 1230513 and self.options.mutliworld_cheato == False) :
             return False
         
-        if(item.code == 1230512 and self.options.multiworld_honeycombs == False) :
+        if((1230703 <= item.code <= 1230727) and self.options.multiworld_honeycombs == False) :
             return False
         
         if(item.code == 1230511 and self.options.multiworld_glowbos == False) :
