299bea7b48a8ba988817adc54b3d6ba8f42ed830
g0goTBC <g0gosigchr@gmail.com>
2024-05-29 16:19:02 -0400

    mcjiggyspecial fix

--- worlds/banjo_tooie/Rules.py
+++ worlds/banjo_tooie/Rules.py
@@ -3641,7 +3641,7 @@ class BanjoTooieRules:
             raise Exception("Not a BK move! {}".format(move))
         if self.world.options.randomize_bk_moves == 0: # Not randomised
             return True
-        elif move in [itemName.TTROT, itemName.TJUMP]: # McJiggy Special, not randomised.
+        elif self.world.options.randomize_bk_moves == 1 and move not in [itemName.DIVE, itemName.FPAD, itemName.FFLIP, itemName.EGGSHOOT, itemName.ROLL]: # McJiggy Special, not randomised.
             return True
         return state.has(move, self.player)
     
