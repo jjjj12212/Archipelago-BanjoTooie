0e1b2ac03acd935b685218660d3453a63a20f0a5
fhnnhf <160126948+fhnnhf@users.noreply.github.com>
2024-11-22 14:19:26 -0500

    half useful, half filler for extra notes and jiggies

--- worlds/banjo_tooie/__init__.py
+++ worlds/banjo_tooie/__init__.py
@@ -107,14 +107,19 @@ class BanjoTooieWorld(World):
         if banjoItem.type == 'progress':
             if banjoItem.btid == 1230515:
                 maxJiggy = max(self.randomize_worlds.values()) if self.randomize_worlds else 70
-                if self.jiggy_counter > maxJiggy:
+                extraJiggys = (90 - maxJiggy)/2
+                if self.jiggy_counter > (maxJiggy+extraJiggys):
                     item_classification = ItemClassification.filler
+                elif self.jiggy_counter > maxJiggy:
+                    item_classification = ItemClassification.useful
                 else:
                     item_classification = ItemClassification.progression
                 self.jiggy_counter += 1
             elif banjoItem.btid == 1230797 and self.options.randomize_notes.value == True:
-                if self.notecounter > 124:
+                if self.notecounter > 130:
                     item_classification = ItemClassification.filler
+                elif self.notecounter > 117:
+                    item_classification = ItemClassification.useful
                 else:
                     item_classification = ItemClassification.progression
                 self.notecounter += 1
