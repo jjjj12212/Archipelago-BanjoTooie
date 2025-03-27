679b00ab994def96388c060c1f42bfd3bcf1e52e
aXu-AP <1621768+aXu-AP@users.noreply.github.com>
2025-03-27 17:39:14 +0200

    Add secondary prizes to boss location groups
    Jiggy under Terry's nest and Weldar's Cheato page, as they require defeating the bosses.

--- worlds/banjo_tooie/__init__.py
+++ worlds/banjo_tooie/__init__.py
@@ -120,8 +120,10 @@ class BanjoTooieWorld(World):
         locationName.JIGGYGM1,
         locationName.JIGGYWW3,
         locationName.JIGGYJR7,
+        locationName.JIGGYTD1,
         locationName.JIGGYTD4,
         locationName.JIGGYGI2,
+        locationName.CHEATOGI3,
         locationName.JIGGYHP1,
         locationName.JIGGYCC1,
     }
